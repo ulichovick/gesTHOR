ab4e85cdaee50d89f010785422b268c18ed1c9b5
ulichovick <ulichovick@gmail.com>
2023-04-20 23:32:46 -0500

    agregada la lista de repos(pendiente agregar algo con seleccion multiple... o no XD) y conectado a la libreria de dnf(ya busca instalados y en linea :) ) y en caso de que se demore buscando en linea mostrara un spinner

--- gtk_gui.py
+++ gtk_gui.py
@@ -1,4 +1,5 @@
 import gi
+from dnf_test import query_local_packages, query_available_packages
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -8,14 +9,34 @@ class Handler:
     def onDestroy(self, *args):
         Gtk.main_quit()
 
-    def onButtonPressed(self, button):
-        print("Buscando")
+    def onButtonAvailablePressed(self, button):
+        res_avail.clear()
+        available_filters = available_filter.get_text()
+        print(available_filters)
+        available_spinner.start()
+        available_data = query_available_packages(available_filters)
+        for pkg in available_data:
+            res_avail.append([str(pkg)])
+        available_spinner.stop()
+    
+    def onButtonInstalledPressed(self, button):
+        res_install.clear()
+        installed_filters = installed_filter.get_text()
+        print(installed_filters)
+        installed_data = query_local_packages(installed_filters)
+        for pkg in installed_data:
+            res_install.append([str(pkg)])
 
 builder = Gtk.Builder()
 builder.add_from_file("test.glade")
 builder.connect_signals(Handler())
 
 window = builder.get_object("window")
+installed_filter = builder.get_object("installed_filter")
+available_filter = builder.get_object("available_filter")
+available_spinner = builder.get_object("available_spinner")
+res_install = builder.get_object("resultados_installe")
+res_avail = builder.get_object("resultados_availabl")
 
 window.show_all()
 
