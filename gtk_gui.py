e8009181ad9e85c83cc97075764f42c146c6a5a1
ulichovick <ulichovick@gmail.com>
2023-04-25 22:54:06 -0500

    agregada funcionalidad de remover paquetes, pendiente resolver problemas de autenticacion.. opciones:requerir autenticacion al iniciar app o requerir autenticacion cuando se ejecute una transaccion I/U

--- gtk_gui.py
+++ gtk_gui.py
@@ -30,15 +30,6 @@ class Handler:
         thread.daemon = True
         thread.start()
 
-    def onButtonInstalledPressed(self, button):
-        res_install.clear()
-        installed_filters = installed_filter.get_text()
-        installed_spinner.start()
-        installed_data = query_local_packages(installed_filters)
-        for pkg in installed_data:
-            res_install.append([str(pkg)])
-        installed_spinner.stop()
-
     def AddPackages(self, button):
         to_install_pkg = results_avail.get_selection()
         to_install_pkg.connect('changed', self.on_tree_selection_changed)
@@ -90,7 +81,63 @@ class Handler:
         model, treeiter = selection.get_selected()
         if treeiter != None:
             print ("You selected", model[treeiter][0])
-        
+
+
+
+#-----------------------------Espacio reservado para pestaña de desinstalar-----------------------------
+
+
+    def unins_spin(self):
+        desinstalando_spinner.start()
+        label_res_ins.set_text("Instalando")
+        return False
+
+    def onButtonInstalledPressed(self, button):
+        res_install.clear()
+        installed_filters = installed_filter.get_text()
+        installed_spinner.start()
+        installed_data = query_local_packages(installed_filters)
+        for pkg in installed_data:
+            res_install.append([str(pkg)])
+        installed_spinner.stop()
+
+    def add_pkgs_to_uninstall(self, button):
+        to_install_pkg = resultados_instalados.get_selection()
+        to_install_pkg.connect('changed', self.on_tree_selection_changed)
+        (model, pathlist) =  to_install_pkg.get_selected_rows()
+        for path in pathlist :
+            tree_iter = model.get_iter(path)
+            print("to Add", model.get_iter(path))
+            a_desinstalar.append([str(model.get_value(tree_iter,0))])
+
+    def uninstall_in_another_thread(self):
+        GLib.idle_add(self.another_spin)
+        cwd = os.getcwd()
+        cwd = cwd + "/dnf_uninstall.py"
+        pkgs = ' '.join(self.pkg_to_uninstall)
+        print("pkgs to be installed: ", pkgs)
+        try:
+            subprocess.call(['pkexec', "--user", "root", "sudo", "-u", "root", "python3", cwd, "--package", pkgs])
+            resultado_desins.set_text(str("Paquetes desinstalados exitosamente: "+pkgs))
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+        desinstalando_spinner.stop()
+        return True
+
+    def desinstalar_pkgs(self, button):
+        to_uninstall_pkgs = desinstalando.get_selection()
+        to_uninstall_pkgs.connect('changed', self.installing)
+        self.pkg_to_uninstall = []
+        (model, pathlist) =  to_uninstall_pkgs.get_selected_rows()
+        for path in pathlist :
+            tree_iters = model.get_iter(path)
+            print("to install", model.get_iter(path))
+            self.pkg_to_uninstall.append(model.get_value(tree_iters,0))
+        print("package to install", self.pkg_to_uninstall)
+        thread = Thread(target=self.uninstall_in_another_thread)
+        thread.daemon = True
+        thread.start()
+
 
 builder = Gtk.Builder()
 builder.add_from_file("test.glade")
@@ -109,6 +156,15 @@ results_avail = builder.get_object("resultados_avail")
 pkgs_to_install = builder.get_object("resultados_avail_install")
 label_res_ins = builder.get_object("Resultado Ins")
 
+
+#-------------------------------elementos de pestaña de desinstalar-------------------------------
+
+
+resultados_instalados = builder.get_object("pkgs_instalados")
+a_desinstalar = builder.get_object("pkgs_a_desins")
+desinstalando = builder.get_object("pkgs_a_desinstalar")
+resultado_desins = builder.get_object("resultado_desins")
+desinstalando_spinner = builder.get_object("desinstalando")
 window.show_all()
 
 Gtk.main()
