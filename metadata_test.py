7bd0b4f9ca9dbd5a95c3f90ede6bc4350949337b
ulichovick <ulichovick@gmail.com>
2023-05-19 22:51:39 -0500

    adding temporal test api file

---
+++ metadata_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import dnf
+
+base = dnf.Base()
+base.read_all_repos()
+base.fill_sack()
+
+q = base.sack.query()
+a = q.available()
+a = a.filter(name='nvidia', latest = 1)
+
+print("Available dnf packages:")
+for pkg in a:  # a only gets evaluated here
+    print('Package {}'.format(pkg))
+    print('Package name {}'.format(pkg.name))
+    print('Repo name {}'.format(pkg.reponame))
+    print('Arch {}'.format(pkg.arch))
+    print('Version {}'.format(pkg.version))
+    print('Description {}'.format(pkg.description))
+    print('FIles {}'.format(pkg.files))
